a67a39e7fd98beadde82af289c83e974419d7224
steven7 <skanceruk3@gatech.edu>
2018-05-18 23:42:55 -0400

    make liverservertestcase stuff

---
+++ functional_tests/tests.py
@@ -0,0 +1,91 @@
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import unittest
+
+class NewVisitorTest(LiveServerTestCase):
+
+	def setUp(self):
+		return
+
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		self.browser.get(self.live_server_url)
+
+
+
+class NewVisitorTest(unittest.TestCase):
+
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+
+		
+	def tearDown(self):
+		self.browser.quit()
+
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		
+		self.browser.get('http://localhost:8020')
+		
+		self.assertIn('To-Do', self.browser.title)
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
+
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'Enter a to-do item'
+			)
+
+		inputbox.send_keys('1: Buy peacock feathers')
+
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly') 
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
+		 
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+		# table = self.browser.find_element_by_id('id_list_table')
+		# rows = table.find_elements_by_tag_name('tr')
+		# self.assertTrue(
+		# 	any(row.text == '1: Buy peacock feathers' for row in rows),
+		# 	f"New to-do item did not appear in table. Contents were:\n{table.text}"
+		# )
+		# self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		# self.assertoIn(
+		# 	# inputbox.get_attribute('placeholder'),
+		# 	# 'Enter a to-do item'
+		# 	[row.text for row in rows]
+		# ) 
+
+		self.fail('Finish the test!')
+
+
+
+	"""docstring for NewVisitorTest"""
+	# def __init__(self, arg):
+	# 	super(NewVisitorTest, self).__init__()
+	# 	self.arg = arg
+		
+
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
+ 
+
+# assert 'To-Do' in browser.title
+
+
+# assert 'Django' in browser.title
+
+# browser.quit()
